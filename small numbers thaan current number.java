00a5fcf59f578ac9e7ff9563d5e5f40809b9e7d2
sweetyroyal123819 <lyaswanthiroyal@gmail.com>
2025-12-29 15:25:02 +0530

    count smaller numbers than current number

---
+++ small numbers thaan current number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int n=nums.length;
+        int current[]=new int[n];
+        for(int i=0;i<n;i++){
+             int count =0;
+            for( int j=0;j<n;j++)
+                if(j!=i && nums[j]<nums[i])
+                count++;
+                current[i]=count;
+        }
+            return current;
+    }
+}
