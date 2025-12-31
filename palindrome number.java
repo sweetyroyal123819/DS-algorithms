759d9e548d3723f4758dda790af687eb240bcfbb
sweetyroyal123819 <lyaswanthiroyal@gmail.com>
2025-12-31 14:21:43 +0530

    palindrome number

---
+++ palindrome number.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0) return false;
+        return new StringBuilder(""+x).reverse().toString().equals(""+x);
+        
+    }
+}
