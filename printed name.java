a71180fa9fb8d0b4dd5a2f5e4d1d2a2c18ae6b2a
sweetyroyal123819 <lyaswanthiroyal@gmail.com>
2026-01-02 12:24:09 +0530

    print user-entered name

---
+++ printed name.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter your name:");
+        String name=sc.next();
+        System.out.println(name+" name is printed");
+    }
+}
