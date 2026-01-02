a6726e8aac7e0c5065d165a665231fe0e6d00e36
sweetyroyal123819 <lyaswanthiroyal@gmail.com>
2026-01-02 11:10:57 +0530

    pyramid pattern
    Implement a pyramid pattern printing program in Java.

---
+++ pyramid pattern.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            for(int j=i+1;j<=5;j++){
+                System.out.print(" ");
+            }
+            for(int k=0;k<=i;k++){
+                System.out.print("& ");
+            }
+            System.out.println();
+        }
+    }
+}
